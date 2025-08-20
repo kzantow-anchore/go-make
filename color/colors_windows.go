4d1fb55054b9d8eda48981ffa06c76bf9c6f0467
Keith Zantow <kzantow@gmail.com>
2025-08-20 08:56:48 -0400

    fix: correct windows terminal color handling
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- color/colors_windows.go
+++ color/colors_windows.go
@@ -1,6 +1,7 @@
 package color
 
 import (
+	"fmt"
 	"os"
 
 	"golang.org/x/sys/windows"
@@ -9,6 +10,11 @@ import (
 func init() {
 	fd := windows.Handle(os.Stderr.Fd()) // os.Stdout?
 	var originalMode uint32
-	windows.GetConsoleMode(fd, &originalMode)
-	windows.SetConsoleMode(fd, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+	err := windows.GetConsoleMode(fd, &originalMode)
+	if err == nil {
+		err = windows.SetConsoleMode(fd, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+	}
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to set console mode: %v\n", err)
+	}
 }
